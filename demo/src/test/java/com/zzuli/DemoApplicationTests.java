a260652c70112ebd9940e4813d4cc01bfe11fbc4
liugang <297980630@qq.com>
2020-01-08 09:03:22 +0800

    这是git的第一次测试的提交方式

---
+++ demo/src/test/java/com/zzuli/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzuli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
